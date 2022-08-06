1b59be612d1f4d7d3db95dc2aa6f605ef58d6d4b
Christophe Ramananjaona <chris@sesame.fi>
2022-08-06 10:24:27 +0200

    Modifications for deployment on Github Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./home";
 import Resultat from "./resultat";
 import Passe from "./passe";
